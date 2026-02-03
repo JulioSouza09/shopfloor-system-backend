0954b9078ee0602563dda9a8835b1103310d4a00
Julio Souza <juliosouza.ux@gmail.com>
2026-02-03 12:08:24 +0000

    refactor: wrap machines controllers into try/catch blocks

--- src/controllers/machinesControllers.js
+++ src/controllers/machinesControllers.js
@@ -1,28 +1,46 @@
 import machinesService from "../services/machinesService.js";
 
 export const getAllMachines = (req, res) => {
-    res.json(machinesService.getAll());
+    try {
+        res.json(machinesService.getAll());
+    } catch (error) {
+        console.error('Failed to get all machines: ', error);
+        res.status(500).json({ error: "Failed to get all machines" })
+    }
 };
 
 export const createNewMachine = (req, res) => {
-    const newMachine = machinesService.createNew(req.body);
-    res.status(201).json(newMachine);
+    try {
+        const newMachine = machinesService.createNew(req.body);
+        res.status(201).json(newMachine);
+    } catch (error) {
+        console.error('Failed to add new machine: ', error);
+        res.status(500).json({ error: "Failed to add new machine" })
+    }
 };
 
 export const getMachine = (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const machine = machinesService.get(id);
-    if (!machine) {
-        return res.status(404).json({ error: "Machine not found." });
+    try {
+        const machine = machinesService.get(req.params.id);
+        if (!machine) {
+            return res.status(404).json({ error: "Machine not found." });
+        }
+    } catch (error) {
+        console.error('Failed to get machine: ', error);
+        res.status(500).json({ error: "Failed to get machine" })
     }
     res.json(machine);
 };
 
 export const updateMachineStatus = (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const machine = machinesService.updateStatus(id, req.body);
-    if (!machine) {
-        return res.status(404).json({ error: "Machine not found " });
+    try {
+        const machine = machinesService.updateStatus(req.params.id, req.body);
+        if (!machine) {
+            return res.status(404).json({ error: "Machine not found " });
+        }
+        res.json(machine);
+    } catch (error) {
+        console.error('Failed to update machine: ', error);
+        res.status(500).json({ error: "Failed to update machine" })
     }
-    res.json(machine);
 };
